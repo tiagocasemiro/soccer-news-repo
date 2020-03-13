e08e05b04cd18d7d5dc3df5143a07130b49ef01c
Tiago P. Casemiro <tiago.casemiro@m4u.com.br>
2020-03-13 15:50:17 -0300

    Ajustado nome do arquivo gerado

--- src/Application.kt
+++ src/Application.kt
@@ -19,7 +19,6 @@ import java.net.UnknownHostException
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
 fun Application.module() {
     install(ContentNegotiation) {
         gson {
