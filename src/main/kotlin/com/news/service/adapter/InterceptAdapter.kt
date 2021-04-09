936022a1f0c5517bed0628d005710b0e1cb68e01
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-08 21:47:08 -0300

    added new adapters

---
+++ src/main/kotlin/com/news/service/adapter/InterceptAdapter.kt
@@ -0,0 +1,26 @@
+package com.news.service.adapter
+
+import com.news.domain.google.Article
+import com.news.domain.google.Source
+import com.rometools.rome.feed.synd.SyndEntry
+import com.rometools.rome.feed.synd.SyndFeed
+
+class InterceptAdapter(from: SyndFeed) : BaseArticleAdapter<Article>(from) {
+    override fun article(entry: SyndEntry): Article {
+        return Article(
+            source = Source(
+                name = entry.source?.title?:"The Intercept Brasil",
+                description = entry.source?.description,
+                url = entry.source?.link?: "https://theintercept.com",
+                category = entry.source?.categories?.toString()?: "Política",
+                language = entry.source?.language?: "Português"),
+            author = entry.author,
+            title = entry.title,
+            description  = entry.description.value,
+            url  = entry.uri,
+            urlToImage  = entry.foreignMarkup?.let { if(it.size > 2) it[2].getAttribute("url")?.value else null},
+            publishedAt = entry.publishedDate.toString(),
+            content = entry.contents[0].value
+        )
+    }
+}
---
+++ src/main/kotlin/com/news/service/adapter/TechMundoAdapter.kt
@@ -0,0 +1,26 @@
+package com.news.service.adapter
+
+import com.news.domain.google.Article
+import com.news.domain.google.Source
+import com.rometools.rome.feed.synd.SyndEntry
+import com.rometools.rome.feed.synd.SyndFeed
+
+class TechMundoAdapter(from: SyndFeed) : BaseArticleAdapter<Article>(from) {
+    override fun article(entry: SyndEntry): Article {
+        return Article(
+            source = Source(
+                name = entry.source?.title?:"Novidades do TecMundo",
+                description = entry.source?.description?:"Descubra e aprenda tudo sobre tecnologia",
+                url = entry.source?.link?: "http://www.tecmundo.com.br/",
+                category = entry.source?.categories?.toString()?: "Tecnologia",
+                language = entry.source?.language?: "Português"),
+            author = entry.author,
+            title = entry.title,
+            description  = entry.description.value,
+            url  = entry.uri,
+            urlToImage  = entry.enclosures[0].url,
+            publishedAt = entry.publishedDate.toString(),
+            content = entry.description.value
+        )
+    }
+}
