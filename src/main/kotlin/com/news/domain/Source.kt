4a960e2dd81c000a4a705001a63656182cabc5ca
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-23 12:21:50 -0300

    separete the source from adapter

--- src/main/kotlin/com/news/domain/Source.kt
+++ src/main/kotlin/com/news/domain/Source.kt
@@ -17,3 +17,40 @@ data class Sources (
 	@SerializedName("totalResults") var totalResults : Int,
 	@SerializedName("sources") val sources : List<Source>
 )
+
+fun nexoSource() = Source(
+	id = "nexo-jornal",
+	name = "Nexo Jornal",
+	description = "Informação clara e bem explicada você encontra aqui. Nexo, leitura obrigatória para quem quer entender o contexto das principais notícias do Brasil e do mundo.",
+	url = "https://www.nexojornal.com.br",
+	language = "Português",
+	country = "Brasil",
+	category = "general")
+
+fun techMundoSource() = Source(
+	id = "tec-mundo",
+	name = "Novidades do TecMundo",
+	description = "Descubra e aprenda tudo sobre tecnologia",
+	url = "http://www.tecmundo.com.br",
+	category = "Tecnologia",
+	language = "Português")
+
+fun theInterceptBrazilSource() = Source(
+	id = "the-intercept-brazil",
+	name = "The Intercept Brasil",
+	description = "O The Intercept Brasil é uma premiada agência de notícias dedicada à responsabilização dos poderosos por meio de um jornalismo destemido e combativo. Suas investigações aprofundadas e suas análises implacáveis se concentram em política, corrupção, meio ambiente, segurança pública, tecnologia, mídia e muito mais. O The Intercept dá aos seus jornalistas a liberdade editorial e o suporte legal de que precisam para expor a corrupção e a injustiça onde quer que as encontrem",
+	url = "https://theintercept.com",
+	category = "Política",
+	language = "Português")
+
+fun elPaisSource() = Source(
+	id = "el-pais",
+	name = "Edição Brasil no EL PAÍS: o jornal global",
+	description = "Últimas notícias do Brasil e do mundo: política, economia, esportes, cultura e sociedade no EL PAÍS. Além disso, especiais, vídeos, fotos, áudios, gráficos e entrevistas com o EL PAÍS.",
+	url = "https://brasil.elpais.com",
+	category = "General",
+	language = "Português",
+	country = "Brasil")
+
+fun extraSources() = listOf(nexoSource(), techMundoSource(), theInterceptBrazilSource(), elPaisSource())
+
